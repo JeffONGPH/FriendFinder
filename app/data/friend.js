9534bbf5bbfccfaf3cddd408da2208206fc81a08
JeffONGPH <36478153+JeffONGPH@users.noreply.github.com>
2018-08-25 18:55:06 -0400

    friends image

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,68 +1,115 @@
-var friendList = [
-    {
-      name: "Tom",
-      profilePic: "http://oi64.tinypic.com/123lguv.jpg",
-      scores: [
-        "1",
-        "2",
-        "2",
-        "2",
-        "4",
-        "5",
-        "2",
-        "4",
-        "2",
-        "4"
-      ]
-    },
-    {
-        name: "James",
-        profilePic: "http://oi66.tinypic.com/24pdhj5.jpg",
-        scores: [
-          "4",
-          "5",
-          "5",
-          "2",
-          "4",
-          "5",
-          "4",
-          "4",
-          "3",
-          "4"
-        ]
-      },
-      {
-        name: "Harry",
-        profilePic: "http://oi65.tinypic.com/dpg75h.jpg",
-        scores: [
-          "4",
-          "1",
-          "5",
-          "2",
-          "4",
-          "5",
-          "5",
-          "4",
-          "5",
-          "5"
-        ]
-      },
-      {
-        name: "Nancy",
-        profilePic: "http://oi65.tinypic.com/dpgvwy.jpg",
-        scores: [
-          "4",
-          "5",
-          "1",
-          "2",
-          "1",
-          "5",
-          "4",
-          "3",
-          "3",
-          "2"
-        ]
-      }
-  ]
-  
-  module.exports = friendList;
+var friendList = [{
+    "name": "Tom",
+    "profilePic": "http://oi64.tinypic.com/123lguv.jpg",
+    "scores": [
+      "1",
+      "2",
+      "4",
+      "2",
+      "4",
+      "5",
+      "2",
+      "3",
+      "2",
+      "4"
+    ]
+  },
+  {
+    "name": "James",
+    "profilePic": "http://oi66.tinypic.com/24pdhj5.jpg",
+    "scores": [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+  {
+    "name": "Harry",
+    "profilePic": "http://oi65.tinypic.com/dpg75h.jpg",
+    "scores": [
+      "4",
+      "1",
+      "5",
+      "2",
+      "4",
+      "5",
+      "5",
+      "4",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Nancy",
+    "profilePic": "http://oi65.tinypic.com/dpgvwy.jpg",
+    "scores": [
+      "5",
+      "5",
+      "5",
+      "2",
+      "5",
+      "5",
+      "4",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Robot Twin",
+    "profilePic": "http://oi64.tinypic.com/123lguv.jpg",
+    "scores": [
+      "5",
+      "3",
+      "1",
+      "1",
+      "3",
+      "3",
+      "4",
+      "1",
+      "3",
+      "5"
+    ]
+  },
+  {
+    "name": "Johny Bravo",
+    "profilePic": "http://oi41.tinypic.com/30vop5s.jpg",
+    "scores": [
+      "3",
+      "3",
+      "2",
+      "5",
+      "3",
+      "3",
+      "4",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Wiz khafa",
+    "profilePic": "http://oi42.tinypic.com/jh7lf4.jpg",
+    "scores": [
+      "1",
+      "1",
+      "2",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "5"
+    ]
+  }
+]
+
+module.exports = friendList;
